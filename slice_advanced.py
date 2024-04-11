acc3adcfdc1829676b054d49e48657b09af00545
franlu0 <162721582+franlu0@users.noreply.github.com>
2024-04-10 23:09:15 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,7 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
-
+    Txt = "Hello, World!"
+    Txt2 = "12345678910"
+    print(Txt[4:14:2])
+    print(Txt2[4:12:2])
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
